60d12fcdb287d912ff1aa204f3bde9e5a38c21a4
Ultragodop <ultragodop@gmail.com>
2025-09-18 21:46:38 -0300

    Nigga has to be mean to work

--- src/main/java/com/surrogate/Zoolip/models/peticiones/Response.java
+++ src/main/java/com/surrogate/Zoolip/models/peticiones/Response.java
@@ -1,17 +1,24 @@
 package com.surrogate.Zoolip.models.peticiones;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Response {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String status;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String token;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long id;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String httpError;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String username;
 
     public Response(String status, String token, String username, Long id) {
