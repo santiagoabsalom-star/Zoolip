b31c460f2c912a410ea65a46773c68c18ab26594
Ultragodop <ultragodop@gmail.com>
2025-11-14 00:25:41 -0300

    ahi modefoca

--- src/main/java/com/surrogate/Zoolip/models/DTO/PublicacionDTO.java
+++ src/main/java/com/surrogate/Zoolip/models/DTO/PublicacionDTO.java
@@ -1,9 +1,11 @@
 package com.surrogate.Zoolip.models.DTO;
 
 
+import com.surrogate.Zoolip.models.bussiness.Publicacion.Tipo;
+
 import java.time.LocalDateTime;
 
-public record PublicacionDTO(Long idPublicacion,String avatarUrl,long idUsuario, String contenido, String topico, String nombreUsuario, Integer likes,
-                             LocalDateTime fechaDudaResuelta,LocalDateTime fechaPregunta, LocalDateTime fechaEdicion) {
+public record PublicacionDTO(Long idPublicacion, String avatarUrl, long idUsuario, Tipo tipo, String contenido, String topico, String nombreUsuario, Integer likes,
+                             LocalDateTime fechaDudaResuelta, LocalDateTime fechaPregunta, LocalDateTime fechaEdicion) {
 
 }
